c0156e64dfd276e9c0912d85fd647af655a15734
Sasikumar Kalaichelvan <sasikumar_kalaichelvan@epam.com>
2026-01-27 21:55:11 +0530

    done

--- task01/jsrc/main/java/com/task01/HelloWorld.java
+++ task01/jsrc/main/java/com/task01/HelloWorld.java
@@ -11,7 +11,7 @@ import java.util.Map;
 @LambdaHandler(
     lambdaName = "hello_world",
 	roleName = "hello_world-role",
-	isPublishVersion = false,
+	isPublishVersion = true,
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED,
         aliasName = "learn"
 )
